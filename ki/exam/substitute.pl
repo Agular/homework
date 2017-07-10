51b89e8b256f7b73b6f4d9f6b5f0a7b9668a0cf3
Agular <rluga@hot.ee>
2017-07-10 21:52:09 +0200

    The complete solution

--- ki/exam/substitute.pl
+++ ki/exam/substitute.pl
@@ -1,4 +1,5 @@
-subst(X + Y, Answer, A, B):- subst(X, Ans1, A, B), subst(Y, Ans2, A, B), Answer = Ans1 + Ans2.
-subst(X * Y, Answer, A, B):- subst(X, Ans1, A, B), subst(Y, Ans2, A, B), Answer = Ans1 * Ans2.
-subst(X, B, A, B):- X==A.
-subst(X, X, A, _):- X\==A.
+subst1(X + Y, Answer, A, B):- subst1(X, Ans1, A, B), subst1(Y, Ans2, A, B), Answer = Ans1 + Ans2.
+subst1(X * Y, Answer, A, B):- subst1(X, Ans1, A, B), subst1(Y, Ans2, A, B), Answer = Ans1 * Ans2.
+subst1(C * X, C*X, _, _):- number(C).
+subst1(X, Answer, A, B):- atom(X), X==A, Answer = B.
+subst1(X, X, A, _):- (atom(X), X\==A).
